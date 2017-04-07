54abc8fc633b6887bd882769120182e33c9a9f86
李滨澎 <libp@libinpengdeMacBook-Air.local>
2017-04-07 14:27:34 +0800

    c

--- webapp/jetModules/MySQLUtil.js
+++ webapp/jetModules/MySQLUtil.js
@@ -1,24 +1,46 @@
 var mysql = require('mysql');
-
-function MySQLUtil() {
-    var connectionProd = mysql.createConnection({
-        host: 'newsjet-1.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
-        user: 'newsjet',
-        password: 'WcG32wxQVhHK3iI4',
-        database: 'newsjet'
-    });
-    var connectionTest = mysql.createConnection({
-        host: 'newsjet-test3.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
-        user: 'newsjet',
-        password: 'NewsJetTest1234',
-        database: 'newsjet'
-    });
-    this.getConnectionProd = function() {
-        return connectionProd;
-    }
-    this.getConnectionTest = function() {
-        return connectionTest;
-    }
+var testDatabase= {
+    host: 'newsjet-test3.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
+    user: 'newsjet',
+    password: 'NewsJetTest1234',
+    database: 'newsjet'
 };
+var onlineDatabase = {
+    host: 'newsjet-test3.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
+    user: 'newsjet',
+    password: 'NewsJetTest1234',
+    database: 'newsjet'
+}
+var environment = process.env.NODE_ENV || 'online';
+if(environment === 'online'){
+    config = onlineDatabase;
+}else if(environment === 'development'){
+    config = testDatabase;
+}
+var pool = mysql.createPool(config);
+module.exports = pool;
 
-module.exports = MySQLUtil;
+
+
+// function MySQLUtil() {
+//     var connectionProd = mysql.createConnection({
+//         host: 'newsjet-1.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
+//         user: 'newsjet',
+//         password: 'WcG32wxQVhHK3iI4',
+//         database: 'newsjet'
+//     });
+//     var connectionTest = mysql.createConnection({
+//         host: 'newsjet-test3.cluster-cjrqtvo4y0v6.ap-northeast-1.rds.amazonaws.com',
+//         user: 'newsjet',
+//         password: 'NewsJetTest1234',
+//         database: 'newsjet'
+//     });
+//     this.getConnectionProd = function() {
+//         return connectionProd;
+//     }
+//     this.getConnectionTest = function() {
+//         return connectionTest;
+//     }
+// };
+
+// module.exports = MySQLUtil;
