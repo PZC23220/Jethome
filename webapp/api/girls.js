8c288e384025927eb27de7e9f2f045372b96d617
李滨澎 <libp@libinpengdeMacBook-Air.local>
2017-05-22 18:52:46 +0800

    commit pudate

--- webapp/api/girls.js
+++ webapp/api/girls.js
@@ -3,28 +3,27 @@ var router = express.Router();
 var models = require('../models');
 var _ = require('lodash');
 
+// 获取girls list
 router.get('/api/girls/list', function(req, res){
     var data = [];
-    models.girls_album.findAll({
-        where:{},
+    var page = req.query.page || 1;
+    var pageNum = req.query.pageNum || 10;
+    models.girls_album.findAndCount({
+        where:{active: 1},
         attributes: ['id', 'cid'],
-        limit: 10,
+        limit: Number(pageNum),
+        offset: (pageNum * page) - pageNum,
         row: true
     }).then(function(result){
+        console.log(result.count);
         return result;
-        // console.log('test');
-        // console.log(result);
-        // console.log(result.get());
-        // res.send(result);
     }).catch(function(err){
         res.send(err);
     }).then(function(result){
-        var data = result;
-        // return false;
-        // console.log(result);
-        // console.error(result);
-        // data = result.toJSON();
-        var albumidList = _.map(result, function(item, index){
+        var total = result.count;
+        var data = result.rows;
+
+        var albumidList = _.map(data, function(item, index){
             return item.id;
         });
         models.girls_single.findAll({
@@ -51,11 +50,12 @@ router.get('/api/girls/list', function(req, res){
                     }
                 });
             });
-            res.send(data);
+            res.send({total: total, data: data});
         })
     });
 });
 
+// girls category
 router.get('/api/girls/category', function(req, res, next){
     models.girls_category.findAll({
         attributes: ['cid','name_cn']
@@ -67,6 +67,40 @@ router.get('/api/girls/category', function(req, res, next){
         return false;
     });
 });
+
+// 修改girls分类
+router.post("/api/girls/category/edit", function(req, res, next){
+    var data = req.body;
+    models.girls_album.update(data, {
+        where: {id: data.id},
+        fields: ['cid'],
+    }).then(function(result){
+        console.log(result);
+        if(result[0] === 1){
+            res.send({success: true, code: 200, msg: '修改分类成功'});
+        }
+    }).catch(function(err){
+        res.send(err);
+    })
+});
+
+// girls delete
+router.post('/api/girls/delete', function(req, res, next){
+    var data = req.body;
+    models.girls_album.update({active:0}, {
+        where: {id: data.id},
+        fields: ['active']
+    }).then(function(result){
+        console.log(result);
+        if(result[0] === 1){
+            res.send({success: true, code: 200, msg: '删除成功'});
+        }else{
+            res.send({success: false, code: 200, msg: '删除失败'});
+        }
+    }).catch(function(err){
+        res.send(err);
+    })
+})
 
 
 module.exports = router;
